4c8f544ef6fb7782db6836977cf5b94a4c0089f7
Eidnoxon <141496565+PCPPTech@users.noreply.github.com>
2024-02-27 21:11:38 +0100

    Add files via upload

---
+++ info.js
@@ -0,0 +1,45 @@
+function showBackground() {
+    document.getElementById('backgroundTitle').innerText = "━━━━━━━━━━━━━━━━━✦⊱ Background Story about the app ⊰✦━━━━━━━━━━━━━━━━";
+    document.getElementById('backgroundInfo').innerHTML = "Fact: <b>The whole entire app wasn't my idea</b>. My brother was the one who told me this great idea of making an app that is basically a to-do list app, but with more functionalities. He said he could use it for stuff. I installed and setup my program on his computer, and he still uses it. I decided to program and design the app further, and maybe build my own community. I am still currently working on building my own community. Not with the best luck or progress, but somehow im still doing it.";
+    document.getElementById('backgroundInformation2').innerHTML = "";
+}
+
+function showUse() {
+    document.getElementById('backgroundTitle').innerText = "━━━━━━━━━━━━━━━━━━✦⊱ How to use VarMaker™ ⊰✦━━━━━━━━━━━━━━━━━━━━";
+    document.getElementById('backgroundInfo').innerHTML = "When you have set it up, double click <b><i>vm.py</i></b>; it is not a virus, it is the application itself. It'll greet you with a platform that asks for a code. In the entry bar, enter in '<b>{@31vZq0/eJ</b>'. I don't see the point of this platform, so I will be removing it soon. After you entered the code, you will be greeted by the menu. Here are some important buttons to know what they do:";
+    document.getElementById('backgroundInformation2').innerHTML = "1. <b>Add Element & Remove Element</b> - <b>Add Element</b> will greet you with a window asking for what element you would like to add to the list. Enter in a random element, and press '<b>Add</b>'. Now close that window, and in VarMaker™ Menu, search for and click '<b>See Table</b>'. You will see your element there. You don't want it to be there anymore? That's why the app has 'Remove Element'. Re-enter the element's name, and it will remove it from the list. The same goes for <b>Add & Remove Value</b>, <b>Add & Remove Date</b>."
+    document.getElementById('bginfo3').innerHTML = "";
+}
+
+function showSetup() {
+    document.getElementById('backgroundTitle').innerText = "━━━━━━━━━━━━━━━━━✦⊱ How To Setup VarMaker™ ⊰✦━━━━━━━━━━━━━━━━━━";
+    document.getElementById('backgroundInfo').innerHTML = "VarMaker has a <b>HOWTOSETUP.txt</b> file which shows instructions on how to set it up. If you don't understand, I'll guide you thru the setup.<br><br> <h3>1. Install VarMaker™.</h3> Install it from <a href='https://github.com/PCPPTech/VarMaker-'><b>GITHUB</b></a> or from VarMaker™'s original website (coming soon). <br> <img src='c:\\Users\\szab9\\OneDrive\\Képek\\Képernyőképek\\gh_code.png', height=400, width=700></img> <hr> <img src='c:\\Users\\szab9\\OneDrive\\Képek\\Képernyőképek\\gh_code2.png'></img>";
+    document.getElementById('backgroundInformation2').innerHTML = "<h3>2. Install Python.</h3> Since the whole application was made 100% with python, you're required to download it in order to use the app. Download Python Installer <a href='c:\\Users\\szab9\\OneDrive\\Asztali gép\\Ne torold ki\\python-3.12.0-amd64.exe'><b>HERE</b></a>. When you're done, double click the python installer. At the beginning you'll see a box that says <b><i>Add python.exe to PATH</i></b>. Check that box, and click <b>Install Now.</b> <br> <img src='c:\\Users\\szab9\\OneDrive\\Képek\\Képernyőképek\\python_installation.png'></img>";
+    document.getElementById('bginfo3').innerHTML = "<h3>3. Setup VarMaker™.</h3> Since I've got countless amount of complaints of the app being really hard to setup, i decided to make the setup as easy as possible to setup. Now that you have python in your path, and installed, extract and double click the VM folder you downloaded. Inside, you'll see a file named <b>vm_setup.py</b>. Double click it. <img src='c:\\Users\\szab9\\OneDrive\\Képek\\Képernyőképek\\setup_guide.png'></img><br> Once you see a window pop up, the setup was successful. Now you're free to use VarMaker™. Double click <b>vm.py</b>, enter in the code <b>{@31vZq0/eJ</b>, and enjoy! Altough I see the code enter part very pointless, I'll remove it next update!";
+}
+
+function showWhy() {
+    document.getElementById('backgroundTitle').innerText = "━━━━━━━━━━━━━━━━━✦⊱ Why Use VarMaker™? ⊰✦━━━━━━━━━━━━━━━━━━━━";
+    document.getElementById('backgroundInfo').innerHTML = "<b>VarMaker™ is a very fast, effecient and powerful tool. It's like a to-do list, but far more advanced.</b><img src='c:\\Users\\szab9\\OneDrive\\Képek\\Képernyőképek\\.vm_guide.png'></img> <br> <b>As you can see on the image, the not only have elements, like normal to-do list type of projects, but also Values AND Dates. You can make your grocery list, gym excersises list, etc. etc. via this app! It also remembers the data you put on the table; basically saves it. We have functionalities like Save, Load and Remove a saved file. <br> <img src='c:\\Users\\szab9\\OneDrive\\Képek\\Képernyőképek\\.vm_guide_saves.png'></img><br> <i>Save a file</i> - Pick a location for your file (file1, 2, etc.). If you dont see a window pop up, everything is fine. It is saved. Your elements, values and dates you entered are saved in a folder. <br> <i>Load Save</i> - Select a file save you saved your elements, values & dates in, and click it. If a window doesn't appear, it's fine, like always. The operation is successful. You have loaded a save file. <br> <i>Remove File</i> - Select a file save where you saved your elements, values and dates that you find useless, and it will delete it. Like always, no window pop-ups, so no worries.</b>";
+    document.getElementById('backgroundInformation2').innerHTML = "";
+    document.getElementById('bginfo3').innerHTML = "";
+}
+function showUpdates() {
+    document.getElementById('backgroundTitle').innerText = "━━━━━━━━━━━━━━━━━━━━━━━✦⊱ UPDATES ⊰✦━━━━━━━━━━━━━━━━━━━━";
+    document.getElementById('backgroundInfo').innerHTML = "<b>2023.12.25</b><br>• Revamped the menu for a more elegant look.<br>• Integraded paddings between buttons to make them more readable.<br><br><b>2024.01.04</b><br>• Fixed bugs occuring with icon images and buttons.<br>• Integraded more button functionalities.<br><br><b>2024.01.14</b><br>• Integraded more button functionalities.<br>• Integraded saving system.<br><br><b>2024.01.18</b><br>• Integraded 'load a saved file' system.<br>• Integraded a 'remove a saved file' system.<br><br><b>2024.01.23</b><br>• Fixed errors occuring with the 'Remove a saved file' system.<br>• Fixed errors occuring with the 'Load a Saved File' system.<br>• Integraded a website via wordpress.com for the application.<br>• Integraded ToS for the application.<br>• Changed 'News' button's name to 'App Progress'.<br>• Fixed the 'Different Colored Buttons' error.<br>• Global release of VarMaker™.<br><br><b>2024. 02. 18</b><br>• Removed Wordpress website, and developed official website.<br>• Removed the code input field.<br>• Made an easier setup for the application.<br>• Developed a discord server for the application. You can check it out <b><a href='https://discord.gg/86vhpkNaa8'>HERE</a></b>"
+    document.getElementById('backgroundInformation2').innerHTML = "";
+    document.getElementById('bginfo3').innerHTML = "";
+}
+
+function showContacts() {
+    document.getElementById('backgroundTitle').innerText = "━━━━━━━━━━━━━━━━━━━━━━━✦⊱ Contacts ⊰✦━━━━━━━━━━━━━━━━━━━━";
+    document.getElementById('backgroundInfo').innerHTML = "<h2><b><a href='https://discord.gg/86vhpkNaa8'>Discord</a></b></h2> <br> <h2><b><a href='www.gmail.com'>Email (eidnoxon213@gmail.com)</a></b></h2>";
+    document.getElementById('backgroundInformation2').innerHTML = "";
+    document.getElementById('bginfo3').innerHTML = "";
+}
+function fastTravel() {
+    document.getElementById('backgroundTitle').innerText = "━━━━━━━━━━━━━━━━━━━━━━✦⊱ Fast Travel ⊰✦━━━━━━━━━━━━━━━━━━━━━";
+    document.getElementById('backgroundInfo').innerHTML = "<b><a>• VarMaker Menu</a></b> <br> COMING SOON....";
+    document.getElementById('backgroundInformation2').innerHTML = "";
+    document.getElementById('bginfo3').innerHTML = "";
+}
